46fec658a292d14830fa492b4fa7614eae14161a
sgilmo <steveg@sgilmo.com>
2025-03-27 14:17:39 -0500

    Refactor database update logic and improve organization
    Replaced direct calls to `sqlserver` with `sql_funcs` for better modularity and readability. Adjusted the order of function calls for consistency and removed redundant operations to streamline execution flow. Added import for `sql_funcs` to support the new structure.

--- spares.py
+++ spares.py
@@ -6,19 +6,20 @@ Also, process engineering requested spare parts and track them through the syste
 import as400
 import sqlserver
 import reqspare
+import sql_funcs
 
 
 def main():
     """Main Function"""
     as400_dbresult = as400.get_inv()
-    sqlserver.update_dbusage(as400.get_usage())
-    sqlserver.update_emps(as400.get_emps())
-    sqlserver.cleanup_pending()
+    sql_funcs.update_dbinv(as400.build_inv_list(as400_dbresult))
+    sqlserver.check_obs(as400_dbresult)
+    sql_funcs.update_dbusage(as400.get_usage())
+    sql_funcs.update_emps(as400.get_emps())
     reqspare.make_req()  # Build and Send email to purchasing
     sqlserver.move_entered_spares()  # Check if part entered on AS400
     sqlserver.move_comp_spares()  # Record stocked requested spare to its own table
-    sqlserver.check_obs(as400_dbresult)
-    sqlserver.update_dbinv(as400.build_inv_list(as400_dbresult))
+
     changed = sqlserver.check_reorder_pts()
     if len(changed) > 0:
         sqlserver.log_changed_reorderpts(changed)
