e786e6797900e1bf3703c1bd8c00e07e0fa76ca6
sgilmo <steveg@sgilmo.com>
2025-09-29 12:38:06 -0500

    Add `update_dbprod` and integrate `tblProd` syncing logic
    Introduced `update_dbprod` to process AS400 production data and implemented `sync_usage` for syncing `tblProd_temp` to `FPMGFILE`. Enhanced spares data flow with improved table-specific operations.

--- spares.py
+++ spares.py
@@ -14,10 +14,14 @@ def main():
     as400_dbresult = as400.get_inv()
     sql_funcs.update_dbinv(as400.build_inv_list(as400_dbresult))
     sqlserver.check_obs(as400_dbresult)
-    sql_funcs.update_dbusage(as400.get_usage())
+
     # Add only new records to tblUsage
+    sql_funcs.update_dbusage(as400.get_usage())
     sql_funcs.sync_usage(schema="dbo", src_table="tblUsage_temp", dst_table="tblUsage")
-    # sql_funcs.update_emps(as400.get_emps())
+    # Add only new records to tblProd
+    sql_funcs.update_dbprod(as400.get_prod())
+    sql_funcs.sync_usage(schema="eng", src_table="tblProd_temp", dst_table="FPMGFILE")
+
     reqspare.make_req()  # Build and Send email to purchasing
     sqlserver.move_entered_spares()  # Check if part entered on AS400
     sqlserver.move_comp_spares()  # Record stocked requested spare to its own table
